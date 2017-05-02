bd73b150170af0d62cecd663828a92dc7f5966c7
LeeKennedy <mail@leekennedy.id.au>
2017-05-02 20:06:43 +1000

    First blush.

---
+++ scripts/restore_comments.R
@@ -0,0 +1,59 @@
+# Clean Up environment ---------------------------------------------------
+rm(list=ls())
+
+# Packages ---------------------------------------------------------------
+library(readxl)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(xlsx)
+
+# Data Input -------------------------------------------------------------
+
+file.remove("Weekly_Old.xlsx")
+file.copy("Weekly.xlsx", "Weekly_Old.xlsx")
+file.remove("Weekly.xlsx")
+
+new_list <- loadWorkbook("~/Desktop/Project List/Weekly_List_Template.xlsx")
+
+new_input <- read.csv("~/Desktop/Project List/project_list.csv")
+
+old_list <- read_excel("~/Desktop/Project List/Weekly_Old.xlsx")
+
+old_list <- old_list[,c(3,4)]
+
+new_1 <- merge(x=new_input, y= old_list, by.x = "Project", by.y = "Project", all = TRUE)
+
+new_1 <- new_1[,c(2,3,1, 11, 5:10)]
+
+colnames(new_1)[4] <- "Comment"
+
+new_1[is.na(new_1)] <- ""
+
+new_1 <- new_1 %>% 
+        arrange(key)
+new_1 <- new_1[-1,]
+
+new_s <- getSheets(new_list)
+
+addDataFrame(new_1, sheet = new_s$List, row.names = FALSE, col.names = FALSE, startRow=2, startColumn = 1) 
+
+
+# write data to sheet starting on line 1, column 4
+saveWorkbook(new_list, "Weekly.xlsx")
+
+temp_list <- read_excel("~/Desktop/Project List/Weekly.xlsx")
+
+temp_list <- temp_list[,c(2:4)]
+temp_list[is.na(temp_list)] <- ""
+
+temp_list <- temp_list %>% 
+        filter(Area != "Done")
+
+write.xlsx(temp_list, "LK_weekly_list.xlsx")
+
+
+
+
+
